b0de1ca3c87b5c55fabefe1443e55ea6e6b2e77f
Ashkan Shakiba <AshkanShakiba11@gmail.com>
2021-09-01 21:53:10 +0430

    Completing header component and Adding button component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  title: string = 'Task Tracker';
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  toggleAddTask() {
+    console.log("Toggle");
+  }
 }
