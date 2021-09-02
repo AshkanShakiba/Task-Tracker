b3eb601c59764d6ca3dbf7e874201a6ca6af0193
Ashkan Shakiba <AshkanShakiba11@gmail.com>
2021-09-02 21:49:24 +0430

    Adding http client to TaskService instead of mock data

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,16 +1,24 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {Task} from '../Task';
 import {TASKS} from '../mock-tasks';
 import {Observable, of} from "rxjs";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {AppModule} from "../app.module";
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
+  API_URL: string;
+  private http: HttpClient;
 
-  constructor() { }
+  constructor(http: HttpClient) {
+    this.http = http;
+    this.API_URL = "http://127.0.0.1:4321/tasks";
+  }
 
   getTasks(): Observable<Task[]> {
-    return of(TASKS);
+    //return of(TASKS);
+    return this.http.get<Task[]>(this.API_URL);
   }
 }
