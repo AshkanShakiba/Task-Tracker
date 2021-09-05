c20e0a28a5640a0a010d70a169062eb8484f74b3
Ashkan Shakiba <AshkanShakiba11@gmail.com>
2021-09-05 19:19:37 +0430

    Adding about component and router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 import {HttpClientModule} from "@angular/common/http";
 import {FormsModule} from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { ButtonComponent } from './components/button/button.component';
-import { TasksComponent } from './components/tasks/tasks.component';
-import { TaskComponent } from './components/task/task.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { AddComponent } from './components/add/add.component';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './components/header/header.component';
+import {ButtonComponent} from './components/button/button.component';
+import {TasksComponent} from './components/tasks/tasks.component';
+import {TaskComponent} from './components/task/task.component';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import {AddComponent} from './components/add/add.component';
 import {ReactiveFormsModule} from "@angular/forms";
+import {RouterModule, Routes} from "@angular/router";
+import { AboutComponent } from './components/about/about.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+let routes: Routes = [
+  {
+    path: "",
+    component: TasksComponent
+  },
+  {
+    path: "about",
+    component: AboutComponent
+  }
+]
 
 @NgModule({
   declarations: [
@@ -19,16 +33,20 @@ import {ReactiveFormsModule} from "@angular/forms";
     ButtonComponent,
     TasksComponent,
     TaskComponent,
-    AddComponent
+    AddComponent,
+    AboutComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     FontAwesomeModule,
     HttpClientModule,
     ReactiveFormsModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(routes, {enableTracing: true})
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
